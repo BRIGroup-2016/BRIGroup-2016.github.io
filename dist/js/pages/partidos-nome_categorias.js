801277de49d72c4c70c763444f80276d4b5bd3f1
Gabriel Almeida <gabriel.almeida.cp2@gmail.com>
2017-05-05 20:43:43 -0300

    melhorias no wordcloud de politicos

--- dist/js/pages/partidos-nome_categorias.js
+++
@@ -1,3 +0,0 @@
-var partidos = ['DEM', 'PCDOB', 'PDT', 'PHS', 'PMB', 'PMDB', 'PP', 'PPS', 'PR', 'PRB', 'PROS', 'PSB',
- 'PSC', 'PSD', 'PSDB', 'PSOL', 'PT', 'PTB', 'PTDOB', 'PTN', 'PV', 'REDE', 'SD',
- 'SEM PARTIDO'];
