652b49d19dcb4c8b852aadc8ef912a14691ec4ab
Gabriel Almeida <gabriel.almeida.cp2@gmail.com>
2017-05-14 22:48:47 -0300

    mudanca nos clusters

--- dist/js/data/clusters_partidos.js
+++ dist/js/data/clusters_partidos.js
@@ -1,3 +1,5 @@
-var clusters_partidos = [['DEM', 'PMDB', 'PP', 'PPS', 'PSD', 'PSDB', 'PTB', 'SD'],
-    ['PCDOB', 'PDT', 'PROS', 'PSB', 'PSOL', 'PT'],
-    ['PHS', 'PMB', 'PR', 'PRB', 'PSC', 'PTDOB', 'PTN', 'PV', 'REDE', 'SEM PARTIDO']];
+var clusters_partidos = [['PDT', 'PMDB', 'PP', 'PRB', 'PSB', 'PSD', 'PTB'],
+						['PHS', 'PROS', 'REDE'],
+						['DEM', 'PPS', 'PSDB', 'SD'],
+						['PCDOB', 'PSOL', 'PT'],
+						['PMB', 'PR', 'PSC', 'PTDOB', 'PTN', 'PV', 'SEM PARTIDO']];
